12897ecfe2f0d67ab925d6b7f49e035f7a177b89
dgross881 <dgross881@gmail.com>
2014-06-27 18:10:51 +0800

    add compress

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,5 @@ Rails.application.configure do
   config.serve_static_assets = true
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   config.assets.compile = true
+  config.assets.compress = true
 end
